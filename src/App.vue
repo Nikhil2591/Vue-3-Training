7af6f265cdd291dfad1f90f99747d1e7c7312902
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2021-02-08 12:04:59 +0000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> {{ title }}</h1>
+    <p>Welcome ...</p>
+    <div v-if="showModal">
+      <Modal theme="" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Giveaway</h1>
+        <p>Grab your ninja swag for half price</p>
+      </Modal>
+    </div>
+    <button @click.alt="toggleModal">open modal (alt)</button>
+    <div v-if="showModalTwo">
+      <Modal theme="" @close="toggleModalTwo">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Giveaway</h1>
+        <p>Grab your ninja swag for half price</p>
+      </Modal>
+    </div>
+    <button @click.alt="toggleModalTwo">open modal two (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
@@ -22,5 +57,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
